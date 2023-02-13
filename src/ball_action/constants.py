512b3be807ac4e92c0454f3e0e5570449f505064
Ruslan Baikulov <ruslan1123@gmail.com>
2023-02-14 00:09:16 +0300

    feat: Add ball action subpackage

---
+++ src/ball_action/constants.py
@@ -0,0 +1,24 @@
+from src.constants import data_dir, soccernet_dir
+
+ball_action_dir = data_dir / "ball_action"
+experiments_dir = data_dir / "experiments"
+
+ball_action_soccernet_dir = soccernet_dir / "spotting-ball-2023"
+
+train_split = [
+    "england_efl/2019-2020/2019-10-01 - Leeds United - West Bromwich",
+    "england_efl/2019-2020/2019-10-01 - Hull City - Sheffield Wednesday",
+    "england_efl/2019-2020/2019-10-01 - Brentford - Bristol City",
+    "england_efl/2019-2020/2019-10-01 - Blackburn Rovers - Nottingham Forest",
+]
+val_split = [
+    "england_efl/2019-2020/2019-10-01 - Middlesbrough - Preston North End",
+]
+test_split = [
+    "england_efl/2019-2020/2019-10-01 - Stoke City - Huddersfield Town",
+    "england_efl/2019-2020/2019-10-01 - Reading - Fulham",
+]
+challenge_split = [
+    "england_efl/2019-2020/2019-10-02 - Cardiff City - Queens Park Rangers",
+    "england_efl/2019-2020/2019-10-01 - Wigan Athletic - Birmingham City",
+]
