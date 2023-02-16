e44e1d4e0959eebc9745141172e29fb0a5c57361
Ruslan Baikulov <ruslan1123@gmail.com>
2023-02-16 12:32:25 +0300

    feat: Stack frames with step

--- src/ball_action/samplers.py
+++ src/ball_action/samplers.py
@@ -7,11 +7,15 @@ from src.frame_fetcher import FrameFetcher
 
 
 class ActionBallSampler(AbstractSampler, metaclass=abc.ABCMeta):
-    def __init__(self, num_frames: int, target_gauss_scale: float):
-        self.num_frames = num_frames
+    def __init__(self, frame_stack_size: int, frame_stack_step: int, target_gauss_scale: float):
+        self.frame_stack_size = frame_stack_size
+        self.frame_stack_step = frame_stack_step
         self.target_gauss_scale = target_gauss_scale
-        self.behind_num_frames = self.num_frames // 2
-        self.ahead_num_frames = self.num_frames - self.behind_num_frames - 1
+
+        self.behind_frames = self.frame_stack_size // 2
+        self.ahead_frames = self.frame_stack_size - self.behind_frames - 1
+        self.behind_frames *= self.frame_stack_step
+        self.ahead_frames *= self.frame_stack_step
 
         self.videos_data = None
         self.num_videos = None
@@ -33,19 +37,20 @@ class ActionBallSampler(AbstractSampler, metaclass=abc.ABCMeta):
     def sample_frame_indexes(self, frame_index):
         return list(
             range(
-                frame_index - self.behind_num_frames,
-                frame_index + self.ahead_num_frames + 1
+                frame_index - self.behind_frames,
+                frame_index + self.ahead_frames + 1,
+                self.frame_stack_step,
             )
         )
 
 
 class TrainActionBallSampler(ActionBallSampler):
-    def __init__(self, num_frames, target_gauss_scale, num_samples):
-        super().__init__(num_frames, target_gauss_scale)
-        self.num_samples = num_samples
+    def __init__(self, frame_stack_size, frame_stack_step, target_gauss_scale, epoch_size):
+        super().__init__(frame_stack_size, frame_stack_step, target_gauss_scale)
+        self.epoch_size = epoch_size
 
     def __len__(self):
-        return self.num_samples
+        return self.epoch_size
 
     def sample(self, index: int):
         assert self.videos_data is not None
@@ -55,8 +60,8 @@ class TrainActionBallSampler(ActionBallSampler):
         video_data = self.videos_data[video_index]
         video_frame_count = video_data["frame_count"]
         frame_index = random.randrange(
-            self.behind_num_frames,
-            video_frame_count - self.ahead_num_frames
+            self.behind_frames,
+            video_frame_count - self.ahead_frames
         )
         frame_indexes = self.sample_frame_indexes(frame_index)
         self.frame_fetcher.init_video(video_data["video_path"], video_frame_count)
