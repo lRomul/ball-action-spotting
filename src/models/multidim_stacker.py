bd79850d64609af12a0a5c71fd53d64c6aa31401
Ruslan Baikulov <ruslan1123@gmail.com>
2023-03-19 00:38:55 +0300

    feat: Generalized mean pooling

--- src/models/multidim_stacker.py
+++ src/models/multidim_stacker.py
@@ -5,16 +5,49 @@ Original idea:
 https://www.kaggle.com/competitions/dfl-bundesliga-data-shootout/discussion/359932
 """
 
+import torch
 from torch import nn
 
 import timm
 from timm.models.layers import (
     DropPath,
     create_conv2d,
-    create_classifier,
     get_act_layer,
     get_norm_act_layer,
 )
+
+
+class GeneralizedMeanPooling(nn.Module):
+    """Applies a 2D power-average adaptive pooling over an input signal composed of several input planes.
+    The function computed is: :math:`f(X) = pow(sum(pow(X, p)), 1/p)`
+        - At p = infinity, one gets Max Pooling
+        - At p = 1, one gets Average Pooling
+    The output is of size H x W, for any input size.
+    The number of output features is equal to the number of input planes.
+    Source: https://github.com/feymanpriv/DELG/blob/master/model/resnet.py
+    Args:
+        output_size: the target output size of the image of the form H x W.
+                     Can be a tuple (H, W) or a single H for a square image H x H
+                     H and W can be either a ``int``, or ``None`` which means the size will
+                     be the same as that of the input.
+    """
+
+    def __init__(self, norm, output_size: int = 1, eps: float = 1e-6):
+        super(GeneralizedMeanPooling, self).__init__()
+        assert norm > 0
+        self.p = nn.Parameter(torch.ones(1) * norm)
+        self.output_size = output_size
+        self.eps = eps
+
+    def forward(self, x):
+        x = x.clamp(min=self.eps).pow(self.p)
+        x = torch.nn.functional.adaptive_avg_pool2d(x, self.output_size).pow(1. / self.p)
+        return x.view(x.size(0), -1)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + str(self.p) + ', ' \
+               + 'output_size=' + str(self.output_size) + ')'
 
 
 class BatchNormAct3d(nn.Module):
@@ -171,9 +204,8 @@ class MultiDimStacker(nn.Module):
             norm_act_layer(num_3d_stack_proj, inplace=True),
         )
 
-        self.global_pool, self.classifier = create_classifier(
-            self.num_features, num_classes, pool_type="avg"
-        )
+        self.global_pool = GeneralizedMeanPooling(3.0)
+        self.classifier = nn.Linear(self.num_features, num_classes, bias=True)
 
     def forward_2d(self, frames):
         b, t, h, w = frames.shape  # (2, 15, 736, 1280)
