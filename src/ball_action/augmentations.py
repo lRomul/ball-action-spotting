a1c51b29b2ddeadf993c1b362e75cef16685170c
Ruslan Baikulov <ruslan1123@gmail.com>
2023-03-21 01:14:48 +0300

    Train experiment augmentations_001

--- src/ball_action/augmentations.py
+++ src/ball_action/augmentations.py
@@ -6,8 +6,8 @@ def get_train_augmentations(size: tuple[int, int]) -> nn.Module:
     size = size[::-1]
     ratio = size[0] / size[1]
     transforms = nn.Sequential(
-        augm.RandomAffine(degrees=(-10, 10), p=0.5),
-        augm.RandomResizedCrop(size, scale=(0.8, 1.0), ratio=(ratio - 0.2, ratio + 0.2), p=1.0),
+        augm.RandomAffine(degrees=(-5, 5), p=0.5),
+        augm.RandomResizedCrop(size, scale=(0.9, 1.0), ratio=(ratio - 0.1, ratio + 0.1), p=1.0),
         augm.RandomHorizontalFlip(p=0.5),
         augm.RandomBrightness(brightness=(0.8, 1.2), p=0.5),
         augm.RandomContrast(contrast=(0.8, 1.2), p=0.5),
