adefc67d8da24f357720988a5c6dc358711de108
Ruslan Baikulov <ruslan1123@gmail.com>
2023-03-28 00:07:01 +0300

    Train experiment regularization_009

--- src/ball_action/augmentations.py
+++ src/ball_action/augmentations.py
@@ -83,15 +83,15 @@ def get_train_augmentations(size: tuple[int, int]) -> nn.Module:
     size = size[::-1]
     ratio = size[0] / size[1]
     transforms = nn.Sequential(
-        RandomCameraMove((-2.5, 2.5), (0.1, 0.05), (0.95, 1.05), p=0.3),
-        augm.RandomRotation(degrees=(-2.5, 2.5), p=0.4),
+        RandomCameraMove((-2.5, 2.5), (0.1, 0.05), (0.95, 1.05), p=0.4),
+        augm.RandomRotation(degrees=(-2.5, 2.5), p=0.5),
         augm.RandomResizedCrop(size, scale=(0.9, 1.0), ratio=(ratio - 0.1, ratio + 0.1), p=0.9),
         augm.RandomHorizontalFlip(p=0.5),
-        augm.RandomSharpness(sharpness=1., p=0.3),
-        augm.RandomMotionBlur(kernel_size=11, angle=7.5, direction=1.0, p=0.3),
-        augm.RandomBrightness(brightness=(0.8, 1.2), p=0.4),
-        augm.RandomContrast(contrast=(0.8, 1.2), p=0.4),
-        augm.RandomPosterize(bits=3, p=0.3),
-        augm.RandomGaussianNoise(mean=0., std=0.05, p=0.3),
+        augm.RandomSharpness(sharpness=1., p=0.4),
+        augm.RandomMotionBlur(kernel_size=11, angle=7.5, direction=1.0, p=0.4),
+        augm.RandomBrightness(brightness=(0.8, 1.2), p=0.5),
+        augm.RandomContrast(contrast=(0.8, 1.2), p=0.5),
+        augm.RandomPosterize(bits=3, p=0.4),
+        augm.RandomGaussianNoise(mean=0., std=0.05, p=0.4),
     )
     return transforms
