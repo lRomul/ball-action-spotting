3d827610ad2a26abcf15293cb3c52be581885145
Ruslan Baikulov <ruslan1123@gmail.com>
2023-04-14 00:39:57 +0300

    Train experiment bce_folds_001

--- scripts/ball_action/train.py
+++ scripts/ball_action/train.py
@@ -77,11 +77,7 @@ CONFIG = dict(
             "drop_path_rate": 0.2,
             "act_layer": "silu",
         }),
-        "loss": ("focal_loss", {
-            "alpha": -1.0,
-            "gamma": 1.2,
-            "reduction": "mean",
-        }),
+        "loss": "BCEWithLogitsLoss",
         "optimizer": ("AdamW", {"lr": get_lr(BASE_LR, BATCH_SIZE)}),
         "device": [f"cuda:{i}" for i in range(torch.cuda.device_count())],
         "image_size": IMAGE_SIZE,
