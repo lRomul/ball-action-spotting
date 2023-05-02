953d1ea7ff5f06674a0546ca4c22aff7e60e79a4
Ruslan Baikulov <ruslan1123@gmail.com>
2023-05-02 16:25:40 +0300

    feat: Add games with cards to val split

--- src/action/constants.py
+++ src/action/constants.py
@@ -10,16 +10,22 @@ visualizations_dir = action_dir / "visualizations"
 action_soccernet_dir = soccernet_dir / "action-spotting-2023"
 
 val_games = [
+    'england_epl/2015-2016/2016-01-23 - 20-30 West Ham 2 - 2 Manchester City',
     'england_epl/2016-2017/2016-10-01 - 14-30 Swansea 1 - 2 Liverpool',
     'england_epl/2016-2017/2017-04-09 - 18-00 Everton 4 - 2 Leicester',
+    'europe_uefa-champions-league/2014-2015/2014-11-05 - 22-45 Manchester City 1 - 2 CSKA Moscow',
     'europe_uefa-champions-league/2016-2017/2016-09-28 - 21-45 Napoli 4 - 2 Benfica',
     'europe_uefa-champions-league/2016-2017/2016-10-19 - 21-45 Paris SG 3 - 0 Basel',
     'france_ligue-1/2016-2017/2016-08-21 - 21-45 Paris SG 3 - 0 Metz',
+    'france_ligue-1/2016-2017/2016-09-09 - 21-45 Paris SG 1 - 1 St Etienne',
     'france_ligue-1/2016-2017/2017-05-14 - 22-00 St Etienne 0 - 5 Paris SG',
+    'germany_bundesliga/2015-2016/2015-10-04 - 18-30 Bayern Munich 5 - 1 Dortmund',
     'germany_bundesliga/2016-2017/2016-12-03 - 17-30 Dortmund 4 - 1 B. Monchengladbach',
     'germany_bundesliga/2016-2017/2017-02-25 - 17-30 SC Freiburg 0 - 3 Dortmund',
     'italy_serie-a/2016-2017/2016-08-20 - 19-00 AS Roma 4 - 0 Udinese',
+    'italy_serie-a/2016-2017/2017-01-22 - 22-45 AS Roma 1 - 0 Cagliari',
     'italy_serie-a/2016-2017/2017-05-06 - 19-00 Napoli 3 - 1 Cagliari',
+    'spain_laliga/2014-2015/2015-05-02 - 19-00 Atl. Madrid 0 - 0 Ath Bilbao',
     'spain_laliga/2016-2017/2016-08-21 - 21-15 Real Sociedad 0 - 3 Real Madrid',
     'spain_laliga/2016-2017/2017-05-14 - 21-00 Las Palmas 1 - 4 Barcelona',
 ]
