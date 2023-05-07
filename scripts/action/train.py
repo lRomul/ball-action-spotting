9d94cf67a00644f9dcb813fb07147b656663009e
Ruslan Baikulov <ruslan1123@gmail.com>
2023-05-07 18:19:56 +0300

    Train experiment action_tuning_002

--- scripts/action/train.py
+++ scripts/action/train.py
@@ -100,7 +100,7 @@ CONFIG = dict(
             "act_layer": "silu",
         }),
         "loss": ("focal_loss", {
-            "alpha": -1.0,
+            "alpha": 0.2,
             "gamma": 1.2,
             "reduction": "mean",
         }),
