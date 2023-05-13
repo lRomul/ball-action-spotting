7df26341ccf50cfe571e58ca0af145306ea98a82
Ruslan Baikulov <ruslan1123@gmail.com>
2023-05-13 19:15:23 +0300

    Train experiment ball_finetune_long_002

--- scripts/ball_action/train.py
+++ scripts/ball_action/train.py
@@ -65,7 +65,7 @@ CONFIG = dict(
     metric_accuracy_threshold=0.5,
     num_nvdec_workers=3,
     num_opencv_workers=1,
-    num_epochs=[1, 5],
+    num_epochs=[2, 10],
     stages=["warmup", "train"],
     argus_params={
         "nn_module": ("multidim_stacker", {
